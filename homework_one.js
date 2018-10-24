033c674d12bb61e2e9006eeb04db75ba2e2897cc
kapriole <rod.gisbert@gmail.com>
2018-10-24 21:18:19 +0200

    Create homework_one.js

---
+++ homework_one.js
@@ -0,0 +1,61 @@
+/* 
+A platform for an digital exhibition with a collaborating artists should be set up. somehow.
+
+Classes:
+Platform, Participants, Pieces of Art, Cooperations, Subjects, Exhibitions
+
+Interactions:
+The platform collects the Pieces of Art. 
+The Participants create the Pieces of Art. 
+A Cooperation consists of several Participants. 
+(A Piece of Art can have one subject or many.)
+An exhibition consists of at least two Pieces of Art.
+
+*/ 
+
+class Platform {
+
+  constructor(type, member) {
+	      this.type = type;
+	      this.member = participant;
+        this.art = [];
+  }
+}
+  
+  class Participant {
+
+  constructor(name, age) {
+	this.name = name;
+	this.age = age;
+  }
+  
+  class PiecesofArt {
+
+  constructor(title, subject, number) {
+  this.title = title;
+	this.subject = subject;
+	this.number = y;
+  }
+  
+  class Exhibition {
+
+  constructor(people, art, participant) {
+  this.people = people;
+	this.art = art;
+  this.participants = [];
+  }
+
+}
+
+PlatformMembers() {
+    alert(this.name);
+  }
+  
+  help(meout) {
+  idk(whatImdoing;
+  }
+
+
+let Participant = new Participant("Mi");
+user.sayHi();
+
